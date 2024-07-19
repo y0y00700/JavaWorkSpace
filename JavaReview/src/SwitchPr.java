3ef2fc92d91efc88345bbe1c867b4909d5c6a431
Min <57902790+y0y00700@users.noreply.github.com>
2024-07-19 19:27:30 +0900

    git init
    라인 삭제 + git init
    commit;

---
+++ JavaReview/src/SwitchPr.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SwitchPr {
+
+    static int adder (int a , int b, char op){
+        switch(op) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                return a / b;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Input a: ");
+
+        String [] a = br.readLine().split(" ");
+
+        System.out.println(adder(Integer.parseInt(a[0]), Integer.parseInt(a[2]), a[1].charAt(0)));
+    }
+}
